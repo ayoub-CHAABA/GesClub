40b8e99762d01b75a9d31e571b4962a6d56aa8c4
CHAABA_AYOUB <64574617+ayoub-CHAABA@users.noreply.github.com>
2024-05-15 12:32:03 +0100

    Update test.jsx

--- test.jsx
+++ test.jsx
@@ -48,6 +48,57 @@ const MyOrgChart = () => {
 };
 
 export default MyOrgChart;
+/*****/
+import React from 'react';
+import { OrganizationChart } from 'primereact/organizationchart';
+
+const MyOrgChart = () => {
+    const data = [{
+        label: 'CEO',
+        type: 'person',
+        expanded: true,
+        data: { name: 'Amy Elsner', avatar: 'path_to_ceo_image.jpg' },
+        children: [
+            {
+                label: 'CFO',
+                type: 'person',
+                expanded: true,
+                data: { name: 'Anna Fali', avatar: 'path_to_cfo_image.jpg' },
+                children: [
+                    { label: 'Tax', type: 'department' },
+                    { label: 'Legal', type: 'department' }
+                ]
+            },
+            {
+                label: 'CTO',
+                type: 'person',
+                expanded: true,
+                data: { name: 'Stephen Shaw', avatar: 'path_to_cto_image.jpg' },
+                children: [
+                    { label: 'Operations', type: 'department' },
+                    { label: 'Software Development', type: 'department' }
+                ]
+            }
+        ]
+    }];
+
+    const nodeTemplate = (node) => {
+        if (node.type === 'person') {
+            return (
+                <div>
+                    <img src={node.data.avatar} alt={node.data.name} style={{ width: '32px', verticalAlign: 'middle' }} />
+                    <span style={{ marginLeft: '5px' }}>{node.data.name}</span>
+                </div>
+            );
+        } else {
+            return node.label;
+        }
+    };
+
+    return <OrganizationChart value={data} nodeTemplate={nodeTemplate} style={{ width: '100%' }} />;
+};
+
+export default MyOrgChart;
 // src/components/MyOrgChart.jsx
 import React from 'react';
 import { OrganizationChart } from 'primereact/organizationchart';
